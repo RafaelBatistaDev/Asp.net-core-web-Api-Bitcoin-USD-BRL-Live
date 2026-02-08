ec9801a8d2f7556d385992e4acdef99589859c27
Rafael Batista <rafaelbatistadev@outlook.com.br>
2026-02-08 13:19:17 -0300

    Add: Entity Framework Core com SQLite e migrações

---
+++ Migrations/20260208161858_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moedas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Simbolo = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moedas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moedas");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-using Scalar.AspNetCore; // <--- NÃO ESQUEÇA ESSA LINHA!
+using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore; // Para o UseSqlite
+using API.Data; // Para o seu AppDbContext
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. PRIMEIRO: Configura os serviços (Builder)
 builder.Services.AddControllers();
 builder.Services.AddOpenApi(); 
 
+// 2. CONFIGURA O BANCO: Sempre dentro da parte do builder
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=moedas.db"));
+
 var app = builder.Build();
 
+// 3. DEPOIS: Configura o pipeline (App)
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
